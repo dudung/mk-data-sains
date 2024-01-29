dbd26343ecb225af865c2b129dc8beb9c830f1a4
Sparisoma Viridi <dudung@gmail.com>
2024-01-29 10:59:38 +0700

    Update mmm_2.py

--- code/mmm_2.py
+++ code/mmm_2.py
@@ -1,11 +1,11 @@
 import statistics as stat
 
-dataset_1 = [1, 2, 3, 5, 5, 5, 7, 18, 29]
-mean_1 = stat.mean(dataset_1)
-median_1 = stat.median(dataset_1)
-mode_1 = stat.mode(dataset_1)
+dataset_2 = [1, 2, 3, 5, 5, 5, 7, 18, 29]
+mean_2 = stat.mean(dataset_2)
+median_2 = stat.median(dataset_2)
+mode_2 = stat.mode(dataset_2)
 
-print(dataset_1)
-print("mean =", mean_1)
-print("median =", median_1)
-print("mode =", mode_1)
+print(dataset_2)
+print("mean =", mean_2)
+print("median =", median_2)
+print("mode =", mode_2)
