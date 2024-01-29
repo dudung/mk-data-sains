383ae11dd9ccfdb7126d05fff449f1edf7ca0dab
Sparisoma Viridi <dudung@gmail.com>
2024-01-29 10:45:22 +0700

    Create mmm_3.py

---
+++ code/mmm_3.py
@@ -0,0 +1,11 @@
+import statistics as stat
+
+dataset_1 = [3, 3, 3, 5, 6, 6, 7, 7, 8]
+mean_1 = stat.mean(dataset_1)
+median_1 = stat.median(dataset_1)
+mode_1 = stat.mode(dataset_1)
+
+print(dataset_1)
+print("mean =", mean_1)
+print("median =", median_1)
+print("mode =", mode_1)
