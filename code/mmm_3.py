02f9ca4d94d7aab1a2c764c82eb00faf2e65e51b
Sparisoma Viridi <dudung@gmail.com>
2024-01-29 11:00:10 +0700

    Update mmm_3.py

--- code/mmm_3.py
+++ code/mmm_3.py
@@ -1,11 +1,11 @@
 import statistics as stat
 
-dataset_1 = [3, 3, 3, 5, 6, 6, 7, 7, 8]
-mean_1 = stat.mean(dataset_1)
-median_1 = stat.median(dataset_1)
-mode_1 = stat.mode(dataset_1)
+dataset_3 = [3, 3, 3, 5, 6, 6, 7, 7, 8]
+mean_3 = stat.mean(dataset_3)
+median_3 = stat.median(dataset_3)
+mode_3 = stat.mode(dataset_3)
 
-print(dataset_1)
-print("mean =", mean_1)
-print("median =", median_1)
-print("mode =", mode_1)
+print(dataset_3)
+print("mean =", mean_3)
+print("median =", median_3)
+print("mode =", mode_3)
